e6786b0ae4d060f4c032a56a18c380ee9918c56d
Kayla Perez <kayla.n.perez404@gmail.com>
2024-08-05 15:08:04 -0700

    effects for getting steps and ingredients

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Card from 'react-bootstrap/Card';
+import { useEffect, useState } from 'react';
 
 // not secure!!
 const API_KEY = '961d0e9dd29244c0ad82adaaad1c15dd';
@@ -125,53 +126,10 @@ function RecipeList() {
   );
 }
 
-// function Recipe({ title, image, author, servings, id, readyTime }) {
-//   return (
-//     <li className="recipe-card">
-//       {/* <div>
-//         <img src={image} alt={`${title}`} />
-//       </div>
-//       <div className="recipe-info">
-//         <h2>{title}</h2>
-//         <p>Created by {author}</p>
-//         <div className="recipe-stats">
-//           <div className="servings">
-//             <box-icon name="bowl-hot" color="#888"></box-icon>
-//             <p>{servings} Servings</p>
-//           </div>
-//           <div className="ready-time">
-//             <box-icon name="time-five" color="#888"></box-icon>
-//             <p>{readyTime} Minutes</p>
-//           </div>
-//         </div>
-//       </div> */}
-//       <Card className="text-center">
-//         <Card.Img variant="top" src={image} className="object-fit-cove" />
-//         <Card.Body>
-//           <Card.Title className="mb-3">{title}</Card.Title>
-//           <Card.Subtitle className="text-muted">
-//             Created by {author}
-//           </Card.Subtitle>
-//           <div className="recipe-stats">
-//             <div className="ready-time">
-//               <box-icon name="time-five" color="#888"></box-icon>
-//               <p>{readyTime} Minutes</p>
-//             </div>
-//             <div className="servings">
-//               <box-icon name="bowl-hot" color="#888"></box-icon>
-//               <p>{servings} Servings</p>
-//             </div>
-//           </div>
-//         </Card.Body>
-//       </Card>
-//     </li>
-//   );
-// }
-
 function Recipe({ title, image, author, servings, id, readyTime }) {
   return (
     <li className="d-flex border rounded align-items-start">
-      <img src={image} alt="" />
+      <img className="rounded-start" src={image} alt="" />
       <div
         style={{ height: '230px' }}
         className="recipe-card-info d-flex w-100 flex-column justify-content-between ps-2 pt-2"
@@ -193,153 +151,79 @@ function Recipe({ title, image, author, servings, id, readyTime }) {
   );
 }
 
-const tempInstructions = {
-  name: '',
-  steps: [
-    {
-      number: 1,
-      step: 'Heat your oven to 350 degrees Fahrenheit, and then cook your pasta as directed.',
-      ingredients: [
-        {
-          id: 20420,
-          name: 'pasta',
-          localizedName: 'pasta',
-          image: 'https://spoonacular.com/cdn/ingredients_100x100/fusilli.jpg',
-        },
-      ],
-      equipment: [
-        {
-          id: 404784,
-          name: 'oven',
-          localizedName: 'oven',
-          image: 'https://spoonacular.com/cdn/equipment_100x100/oven.jpg',
-          temperature: {
-            number: 350.0,
-            unit: 'Fahrenheit',
-          },
-        },
-      ],
-    },
-    {
-      number: 2,
-      step: 'Drain.',
-      ingredients: [],
-      equipment: [],
-    },
-    {
-      number: 3,
-      step: 'Shred the cheese.',
-      ingredients: [
-        {
-          id: 1041009,
-          name: 'cheese',
-          localizedName: 'cheese',
-          image:
-            'https://spoonacular.com/cdn/ingredients_100x100/cheddar-cheese.png',
-        },
-      ],
-      equipment: [],
-    },
-    {
-      number: 4,
-      step: 'Add cooked noodles, cheese, sour cream, salt and pepper, and butter to a medium mixing bowl and stir until combined.',
-      ingredients: [
-        {
-          id: 1102047,
-          name: 'salt and pepper',
-          localizedName: 'salt and pepper',
-          image: 'salt-and-pepper.jpg',
-        },
-        {
-          id: 20421,
-          name: 'cooked pasta',
-          localizedName: 'cooked pasta',
-          image: 'fusilli.jpg',
-        },
-        {
-          id: 1056,
-          name: 'sour cream',
-          localizedName: 'sour cream',
-          image: 'sour-cream.jpg',
-        },
-        {
-          id: 1001,
-          name: 'butter',
-          localizedName: 'butter',
-          image: 'butter-sliced.jpg',
-        },
-        {
-          id: 1041009,
-          name: 'cheese',
-          localizedName: 'cheese',
-          image:
-            'https://spoonacular.com/cdn/ingredients_100x100/cheddar-cheese.png',
-        },
-      ],
-      equipment: [
-        {
-          id: 405907,
-          name: 'mixing bowl',
-          localizedName: 'mixing bowl',
-          image:
-            'https://spoonacular.com/cdn/equipment_100x100/mixing-bowl.jpg',
-        },
-      ],
-    },
-    {
-      number: 5,
-      step: 'Add to a 9 x 13 prepared casserole dish, top with extra cheese if desired, and bake for 20 minutes.',
-      ingredients: [
-        {
-          id: 1041009,
-          name: 'cheese',
-          localizedName: 'cheese',
-          image:
-            'https://spoonacular.com/cdn/ingredients_100x100/cheddar-cheese.png',
-        },
-      ],
-      equipment: [
-        {
-          id: 404635,
-          name: 'casserole dish',
-          localizedName: 'casserole dish',
-          image:
-            'https://spoonacular.com/cdn/equipment_100x100/casserole-dish.png',
-        },
-        {
-          id: 404784,
-          name: 'oven',
-          localizedName: 'oven',
-          image: 'https://spoonacular.com/cdn/equipment_100x100/oven.jpg',
-        },
-      ],
-      length: {
-        number: 20,
-        unit: 'minutes',
-      },
-    },
-    {
-      number: 6,
-      step: 'Remove from oven and serve hot.',
-      ingredients: [],
-      equipment: [
-        {
-          id: 404784,
-          name: 'oven',
-          localizedName: 'oven',
-          image: 'https://spoonacular.com/cdn/equipment_100x100/oven.jpg',
-        },
-      ],
-    },
-  ],
-};
+const tempIngredients = [
+  '1 package of Bowtie pasta cook as directed',
+  '1 cup Extra Sharp Cheddar cheese',
+  '1 cup Extra Sharp White Cheddar cheese',
+  '1 tsp salt and pepper',
+  '1/4 cup sour cream',
+  '1/4 cup unsalted butter room temperature',
+  'Fresh Parsley for garnish',
+];
 
-function RecipeDetails() {
+const tempSteps = [
+  'Heat your oven to 350 degrees Fahrenheit, and then cook your pasta as directed.',
+  'Drain.',
+  'Shred the cheese.',
+  'Add cooked noodles, cheese, sour cream, salt and pepper, and butter to a medium mixing bowl and stir until combined.',
+  'Add to a 9 x 13 prepared casserole dish, top with extra cheese if desired, and bake for 20 minutes.',
+  'Remove from oven and serve hot.',
+];
+
+function RecipeDetails({ selectedId }) {
+  const [ingredients, setIngredients] = useState(tempIngredients);
+  const [steps, setSteps] = useState(tempSteps);
+
+  // useEffect(function () {
+  //   async function fetchIngredients() {
+
+  //       const res = await fetch(
+  //         `https://api.spoonacular.com/recipes/715595/information?apiKey=${API_KEY}`
+  //       );
+  //       const data = await res.json();
+  //       const ingredients = data.extendedIngredients.map(
+  //         (ingredient) => ingredient.original
+  //       );
+  //       console.log(ingredients);
+  //       setIngredients(ingredients);
+  //   }
+  //   fetchIngredients();
+  // }, [selectedId]);
+
+  // useEffect(
+  //   function () {
+  //     async function fetchSteps() {
+  //       const res = await fetch(
+  //         `https://api.spoonacular.com/recipes/715595/analyzedInstructions?apiKey=${API_KEY}`
+  //       );
+  //       const [data] = await res.json();
+  //       const steps = data.steps.map((step) => step.step);
+  //       console.log(steps);
+  //       setSteps(steps);
+  //     }
+  //     fetchSteps();
+  //   },
+  //   [selectedId]
+  // );
+
   return (
     <Card>
       <Card.Title>Title</Card.Title>
-      <Card.Subtitle>Created by X</Card.Subtitle>
-      <Card.Body></Card.Body>
+      <Card.Subtitle>Author</Card.Subtitle>
+      <Card.Body>
+        <h5>Ingredients</h5>
+        <ul>
+          {ingredients.map((ingredient) => (
+            <li>{ingredient}</li>
+          ))}
+        </ul>
+        <h5>Instructions</h5>
+        <ol>
+          {steps.map((step) => (
+            <li>{step}</li>
+          ))}
+        </ol>
+      </Card.Body>
     </Card>
   );
 }
