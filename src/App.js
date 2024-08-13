78a4f4e79147b62bb81b349bd85769080f6e9fa3
Kayla Perez <kayla.n.perez404@gmail.com>
2024-08-13 13:41:20 -0700

    styled list container to look nicer

--- src/App.js
+++ src/App.js
@@ -11,54 +11,37 @@ import { useEffect, useState } from 'react';
 // not secure!!
 const API_KEY = '961d0e9dd29244c0ad82adaaad1c15dd';
 
-const tempData = [
-  {
-    title: 'How to Make the Cheesiest Bowtie Mac and Cheese',
-    id: 715595,
-    preperationMinutes: 15,
-    cookingMinutes: 20,
-    readyInMinutes: 35,
-    creditsText: 'pinkwhen.com',
-    servings: 4,
-    image: 'https://img.spoonacular.com/recipes/715595-312x231.jpg',
-  },
-  {
-    title: 'Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs',
-    id: 716429,
-    preperationMinutes: 20,
-    cookingMinutes: 25,
-    readyInMinutes: 45,
-    creditsText: 'Full Belly Sisters',
-    servings: 2,
-    image: 'https://img.spoonacular.com/recipes/716429-312x231.jpg',
-  },
-  {
-    title: 'Easy Cheesy Pizza Casserole',
-    id: 641893,
-    preperationMinutes: null,
-    cookingMinutes: null,
-    readyInMinutes: 45,
-    creditsText: 'foodista.com',
-    servings: 6,
-    image: 'https://img.spoonacular.com/recipes/641893-312x231.jpg',
-  },
-];
-
 export default function App() {
   const [recipe, setRecipe] = useState({});
   const [recipeList, setRecipeList] = useState([]);
 
   const [query, setQuery] = useState('');
 
+  function filterResults(results) {
+    return results.map((recipe) => {
+      const { title, id, creditsText, image, readyInMinutes, servings } =
+        recipe;
+      const filtered = {
+        title,
+        id,
+        creditsText,
+        image,
+        readyInMinutes,
+        servings,
+      };
+      return filtered;
+    });
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
     (async function () {
       const res = await fetch(
-        `https://api.spoonacular.com/recipes/complexSearch?query=${query}&number=3&apiKey=961d0e9dd29244c0ad82adaaad1c15dd&addRecipeInformation=true&instructionsRequired=true&fillIngredients=true`
+        `https://api.spoonacular.com/recipes/complexSearch?query=${query}&number=7&apiKey=961d0e9dd29244c0ad82adaaad1c15dd&addRecipeInformation=true&instructionsRequired=true&fillIngredients=true`
       );
 
       const data = await res.json();
-      setRecipeList(data.results);
+      setRecipeList(filterResults(data.results));
     })();
   }
 
@@ -69,7 +52,7 @@ export default function App() {
   return (
     <div>
       <NavBar onSubmit={handleSubmit} query={query} setQuery={setQuery} />
-      <Container fluid>
+      <Container>
         <Row>
           <Col>
             <RecipeList
@@ -130,7 +113,10 @@ function Button({ extraClass, children, type }) {
 
 function RecipeList({ recipeList, onSelectRecipe }) {
   return (
-    <ul className="list-unstyled">
+    <ul
+      className="list-unstyled overflow-auto border rounded"
+      style={{ height: '90vh' }}
+    >
       {recipeList.map((recipe) => (
         <Recipe
           onSelectRecipe={onSelectRecipe}
@@ -158,10 +144,15 @@ function Recipe({
 }) {
   return (
     <li
-      className="d-flex border rounded align-items-start"
+      className="d-flex border-top border-bottom align-items-start"
       onClick={() => onSelectRecipe({ title, image, author, id })}
     >
-      <img className="rounded-start" src={image} alt="" width={210} />
+      <img
+        className=""
+        src={image}
+        alt=""
+        style={{ height: '20%', width: '40%' }}
+      />
       <div className="recipe-card-info d-flex w-100 flex-column justify-content-between ps-2 pt-2">
         <h4>{title}</h4>
         <h6 className="text-muted pb-2">Created by {author}</h6>
@@ -180,25 +171,6 @@ function Recipe({
   );
 }
 
-const tempIngredients = [
-  '1 package of Bowtie pasta cook as directed',
-  '1 cup Extra Sharp Cheddar cheese',
-  '1 cup Extra Sharp White Cheddar cheese',
-  '1 tsp salt and pepper',
-  '1/4 cup sour cream',
-  '1/4 cup unsalted butter room temperature',
-  'Fresh Parsley for garnish',
-];
-
-const tempSteps = [
-  'Heat your oven to 350 degrees Fahrenheit, and then cook your pasta as directed.',
-  'Drain.',
-  'Shred the cheese.',
-  'Add cooked noodles, cheese, sour cream, salt and pepper, and butter to a medium mixing bowl and stir until combined.',
-  'Add to a 9 x 13 prepared casserole dish, top with extra cheese if desired, and bake for 20 minutes.',
-  'Remove from oven and serve hot.',
-];
-
 function RecipeDetails({ recipe }) {
   const [ingredients, setIngredients] = useState([]);
   const [steps, setSteps] = useState([]);
@@ -216,7 +188,6 @@ function RecipeDetails({ recipe }) {
         const ingredients = data.extendedIngredients.map(
           (ingredient) => ingredient.original
         );
-        console.log(ingredients);
         setIngredients(ingredients);
       }
       fetchIngredients();
@@ -233,7 +204,6 @@ function RecipeDetails({ recipe }) {
         );
         const [data] = await res.json();
         const steps = data.steps.map((step) => step.step);
-        console.log(steps);
         setSteps(steps);
       }
       fetchSteps();
@@ -242,7 +212,7 @@ function RecipeDetails({ recipe }) {
   );
 
   return (
-    <Card>
+    <Card style={{ height: '90vh' }} className="overflow-auto">
       <Card.Title>{title}</Card.Title>
       <Card.Subtitle>{author}</Card.Subtitle>
       <Card.Body>
