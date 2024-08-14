05d3281e8c37d534cd6abefd302b6dd095ecfc50
Kayla Perez <kayla.n.perez404@gmail.com>
2024-08-14 13:32:30 -0700

    updated margin

--- src/App.js
+++ src/App.js
@@ -146,7 +146,10 @@ export default function App() {
 
 function LeftContainer({ children }) {
   return (
-    <div className="overflow-auto border rounded" style={{ height: '90vh' }}>
+    <div
+      className="overflow-auto border rounded mt-3"
+      style={{ height: '85vh' }}
+    >
       {children}
     </div>
   );
