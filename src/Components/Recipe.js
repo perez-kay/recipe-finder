8461414fe000a3a977e97750aca35f2224653fff
Kayla Perez <kayla.n.perez404@gmail.com>
2024-08-16 12:49:47 -0700

    removed 'created by' text

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -19,7 +19,7 @@ export default function Recipe({
       <img className="recipe-img" src={image} alt={title} />
       <div className="recipe-card-info d-flex w-100 flex-column justify-content-between px-3 pt-3 pt-sm-2">
         <h4>{title}</h4>
-        <p className="text-muted pb-2">Created by {author}</p>
+        <p className="text-muted pb-2">{author}</p>
         <RecipeStats readyTime={readyTime} servings={servings} />
       </div>
     </li>
