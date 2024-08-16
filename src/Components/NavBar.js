068480b5660b4340ed17cc19e4df630810495eab
Kayla Perez <kayla.n.perez404@gmail.com>
2024-08-16 12:30:52 -0700

    navbar fully responsive

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,15 +4,17 @@ import Navbar from 'react-bootstrap/Navbar';
 
 export default function NavBar({ onSubmit, query, setQuery }) {
   return (
-    <Navbar>
+    <Navbar className="bg-secondary-subtle m-2 rounded py-3">
       <Container fluid>
-        <Navbar.Brand className="fw-bold">
-          <span>🍽️</span>FoodFinder
+        <Navbar.Brand className="fw-bold fs-4">
+          <span>🍽️</span>
+          <span className="d-none d-sm-inline">FoodFinder</span>
         </Navbar.Brand>
         <SearchBar onSubmit={onSubmit} query={query} setQuery={setQuery} />
-        <Button>
+        {/* <Button>
           <box-icon type="solid" name="fridge" color="white"></box-icon>
-        </Button>
+        </Button> */}
+        <span>&nbsp;</span>
       </Container>
     </Navbar>
   );
@@ -22,16 +24,19 @@ function SearchBar({ onSubmit, query, setQuery }) {
   return (
     <div>
       <form className="search" onSubmit={(e) => onSubmit(e)}>
-        <div className="input-group">
+        <div className="d-flex">
           <input
             value={query}
             onChange={(e) => setQuery(e.target.value)}
-            className="search-bar px-2"
-            type="search"
+            className="form-control search-bar me-2"
+            type="text"
             placeholder="Search for a recipe"
           />
-          <Button extraClass="btn-search" type="submit">
-            Search
+          <Button type="submit">
+            <span className="d-none d-sm-inline">Search</span>
+            <span className="d-flex align-self-center d-sm-none">
+              <box-icon name="search" color="white"></box-icon>
+            </span>
           </Button>
         </div>
       </form>
